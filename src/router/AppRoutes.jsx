e8ba6bc3caeaeb6c8a611f0241ac587372717252
RunyShark <92100746+RunyShark@users.noreply.github.com>
2022-07-13 17:05:17 -0500

    Login

--- src/router/AppRoutes.jsx
+++ src/router/AppRoutes.jsx
@@ -6,6 +6,8 @@ export const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/login" element={<LoginPage />} />
+
+      <Route path="/*" element={<Navigate to="/login" />} />
     </Routes>
   );
 };
