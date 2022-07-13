3236424cad5e0260f464fa00d39dcc29d0f13a16
RunyShark <92100746+RunyShark@users.noreply.github.com>
2022-07-13 17:08:30 -0500

    Login

--- src/router/AppRoutes.jsx
+++ src/router/AppRoutes.jsx
@@ -6,11 +6,11 @@ export const AppRoutes = () => {
   return (
     <Routes>
       {authStatus === "not-authenticate" ? (
-        <Navigate to="/login" />
+        <Route path="/login/*" element={<LoginPage />} />
       ) : (
         <Route path="/adelante" element={<CalendarPages />} />
       )}
-      <Route path="/login" element={<LoginPage />} />
+      <Route path="/*" element={<Navigate to="/login" />} />
     </Routes>
   );
 };
