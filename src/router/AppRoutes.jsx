3f75d368daba41433cf9903d3f0d867a0890d558
RunyShark <92100746+RunyShark@users.noreply.github.com>
2022-07-13 17:35:50 -0500

    LoginSolucion

--- src/router/AppRoutes.jsx
+++ src/router/AppRoutes.jsx
@@ -1,19 +1,17 @@
 import { Route, Routes, Navigate } from "react-router-dom";
 import { CalendarPages, LoginPage } from "../index";
 export const AppRoutes = () => {
-  const authStatus = "authenticate";
+  const authStatus = "not-authenticate";
 
   return (
-    <>
-      <Routes>
-        {!authStatus === "not-authenticate" ? (
-          <Route path="/*" element={<Navigate to="/auth/login" />} />
-        ) : (
-          <Route path="/*" element={<Navigate to="/adelante" />} />
-        )}
-        <Route path="/auth/login" element={<LoginPage />} />
-        <Route path="/adelante" element={<CalendarPages />} />
-      </Routes>
-    </>
+    <Routes>
+      {authStatus === "not-authenticate" ? (
+        <Route path="/auth/*" element={<LoginPage />} />
+      ) : (
+        <Route path="/" element={<CalendarPages />} />
+      )}
+
+      <Route path="*" element={<Navigate to="/auth/login" />} />
+    </Routes>
   );
 };
