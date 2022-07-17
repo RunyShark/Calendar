47ef26f71d51ce4a9e80f518d4d99930a4d19581
RunyShark <92100746+RunyShark@users.noreply.github.com>
2022-07-17 14:26:01 -0500

    fromCalendar

--- src/calenadar/components/CalendarModal.jsx
+++ src/calenadar/components/CalendarModal.jsx
@@ -27,14 +27,52 @@ export const CalendarModal = () => {
       overlayClassName="modal-fondo"
       closeTimeoutMS={200}
     >
-      <h1>Hola munde</h1>
+      <h1> Nuevo evento </h1>
       <hr />
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic soluta
-        accusamus debitis! Fugit nulla voluptates quidem sunt quos ea tempora
-        dignissimos alias libero, accusantium dolores molestias aperiam ducimus
-        mollitia. Ullam!
-      </p>
+      <form className="container">
+        <div className="form-group mb-2">
+          <label>Fecha y hora inicio</label>
+          <input className="form-control" placeholder="Fecha inicio" />
+        </div>
+
+        <div className="form-group mb-2">
+          <label>Fecha y hora fin</label>
+          <input className="form-control" placeholder="Fecha inicio" />
+        </div>
+
+        <hr />
+        <div className="form-group mb-2">
+          <label>Titulo y notas</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Título del evento"
+            name="title"
+            autoComplete="off"
+          />
+          <small id="emailHelp" className="form-text text-muted">
+            Una descripción corta
+          </small>
+        </div>
+
+        <div className="form-group mb-2">
+          <textarea
+            type="text"
+            className="form-control"
+            placeholder="Notas"
+            rows="5"
+            name="notes"
+          ></textarea>
+          <small id="emailHelp" className="form-text text-muted">
+            Información adicional
+          </small>
+        </div>
+
+        <button type="submit" className="btn btn-outline-primary btn-block">
+          <i className="far fa-save"></i>
+          <span> Guardar</span>
+        </button>
+      </form>
     </Modal>
   );
 };
